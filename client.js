ef2b16a537adeb048afc1941c046b927ecfb20a5
Elflanded <shadenitey@Gmail.com>
2021-01-22 04:17:54 +0000

    main stuff, add .gitignore, etc

--- client.js
+++ client.js
@@ -1,6 +1,13 @@
-const { Client, Intents } = require("discord.js");
+const { Intents } = require("discord.js");
+const { CommandoClient } = require("discord.js-commando");
 
-const client = new Client({
+const client = new CommandoClient({,
+  owner: [
+    "474016258019557377",
+    "738604939957239930"
+  ],
+  commandPrefix: "j!",
+  invite: "",
   ws: {
     intents: Intents.NON_PRIVILEGED
   },
